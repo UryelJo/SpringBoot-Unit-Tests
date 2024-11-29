ec93a270c525d131d4a198023dc6861f61e05b77
uryel <uryeljodelucca18@gmail.com>
2024-11-29 10:52:06 -0300

    feat: implementar toda a parte inicial do projeto.

---
+++ src/main/java/tests/testeunitariosspringboot/TesteUnitariosSpringBootApplication.java
@@ -0,0 +1,13 @@
+package tests.testeunitariosspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteUnitariosSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TesteUnitariosSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/tests/testeunitariosspringboot/TesteUnitariosSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package tests.testeunitariosspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesteUnitariosSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
